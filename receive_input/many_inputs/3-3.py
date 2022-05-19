87d634a7531b3eebd213cd844d8d379200f253ac
majokun <jomamiwa@gmail.com>
2022-05-19 22:44:58 +0900

    done

--- receive_input/many_inputs/3-3.py
+++ receive_input/many_inputs/3-3.py
@@ -4,4 +4,4 @@ n = int(input())
 a = list(map(int, input().split()))
 
 for i in range(n):
-    print(a%10)
+    print(a[i]%10)
--- receive_input/many_inputs/3-5.py
+++ receive_input/many_inputs/3-5.py
@@ -4,4 +4,4 @@ n = int(input())
 a = list(map(int, input().split()))
 
 for i in range(n):
-    print(a[n-i])
+    print(a[n-i-1])
